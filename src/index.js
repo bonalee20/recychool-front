eaf4185dd87d1d34e8d9318044af512cd21056ef
junseoch <ljs862200@gmail.com>
2025-12-15 09:01:30 +0900

    feat: styled-components, styled-reset 추가

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import './index.css';
 import App from './App';
 import store from './store';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyle from "./styles/global";
+import theme from "./styles/theme";
+import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
